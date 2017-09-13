e73371ccb61c1deddf696c971a5657fe56f9c7bb
MihailRyabinin <mihail-ryabinin@yandex.ru>
2017-09-13 11:28:44 +0300

    Для оригинальной идеи хватит

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ if __name__ == '__main__':
     data = Mnist()
     X, y = data.getSetTemp()
     cascade_forest = ForestsModel().get_forests()
-    cascade_forest.fit(X[:40000], y[:40000])
-    pred = cascade_forest.predict(X[40000:])
+    cascade_forest.fit(X[:49000], y[:49000])
+    pred = cascade_forest.predict(X[49000:])
     k = 0
     for i in range(len(pred)):
-        if pred[i] == y[i + 40000]:
+        if pred[i] == y[i + 49000]:
             k += 1
     print(float(k/len(pred)))
     # print('y = ', y[9000:9010], 'pred = ', pred[:10])
