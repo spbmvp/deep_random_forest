66097c965573c6edaf447d0f2a4e36ef003588da
Mikhail Popov <spbmvp@gmail.com>
2017-11-06 23:35:02 +0300

    added parallel job and Breast seed

---
+++ BreastLoad.py
@@ -0,0 +1,27 @@
+from random import shuffle
+
+from numpy import array
+
+
+class Breast:
+    def __init__(self):
+        self.classes = []
+        self.labels = []
+        self.loadFile()
+
+    def loadFile(self):
+        file = open('TrainData/breast.txt', 'r')
+        for line in file.readlines():
+            line_array = array(line.split(), dtype=float)
+            self.classes.append(line_array[1:])
+            self.labels.append(int(line_array[0]))
+
+    def getSet(self, count=106):
+        X_set = []
+        y_set = []
+        a = list(range(len(self.classes)))
+        shuffle(a)
+        for i in a[:count]:
+            X_set.append(self.classes[i])
+            y_set.append(self.labels[i])
+        return array(X_set), array(y_set)
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import logging as log
 
 import numpy as np
+from joblib import Parallel, delayed
 from matplotlib.mlab import frange
 
+from BreastLoad import Breast
 from ForestsModel import ForestsModel
 from IonLoad import Ion
 from Seeds import Seeds
@@ -16,36 +18,48 @@ def initLogging(logLevel):
 
 def forestFit(X_train, y_train, X_test, y_test):
     log.info("Start set fitting")
-    for vi in frange(0, 1, 0.1):
-        acc_array = []
-        log.info("vi = %f", vi)
-        for i in range(10):
-            cascade_forest = ForestsModel(n_trees_cf=int(X_train.shape[0]/2)).get_forests()
-            cascade_forest.fit(X_train, y_train, vi = vi, lamda=10**-12)
-            pred = cascade_forest.predict(X_test)
-            k = 0
-            for j in range(len(pred)):
-                if pred[j] == y_test[j]:
-                    k += 1
-            log.debug("Step %d: Accuracy = %f", i, float(k / len(pred)))
-            acc_array.append(float(k / len(pred)))
-        med = np.median(acc_array)
-        mean = np.mean(acc_array)
-        log.info("\n\tMean accuracy = %f\n\tMedian accuracy = %f\n\tMin accuracy = %f\n\tMax accuracy = %f", mean, med, np.min(acc_array), np.max(acc_array))
+    Parallel(n_jobs=56)(delayed(in_parallel)(X_test, X_train, vi, y_test, y_train)for vi in frange(0, 1, 0.01))
     log.info("Finish set fitting")
+
+
+def in_parallel(X_test, X_train, vi, y_test, y_train):
+    acc_array = []
+    for i in range(50):
+        cascade_forest = ForestsModel(n_trees_cf=int(X_train.shape[0] / 2)).get_forests()
+        cascade_forest.fit(X_train, y_train, vi=vi, lamda=10 ** -12)
+        pred = cascade_forest.predict(X_test)
+        k = 0
+        for j in range(len(pred)):
+            if pred[j] == y_test[j]:
+                k += 1
+        log.debug("Step %d: Accuracy = %f", i, float(k / len(pred)))
+        acc_array.append(float(k / len(pred)))
+    med = np.median(acc_array)
+    mean = np.mean(acc_array)
+    log.info("\n\tvi = %f\n\tMean accuracy = %f\n\tMedian accuracy = %f\n\tMin accuracy = %f\n\tMax accuracy = %f",vi, mean, med,
+             np.min(acc_array), np.max(acc_array))
 
 
 if __name__ == '__main__':
     initLogging(log.INFO)
 
-    data = Ion()
-    n_set = 351
+    data = Breast()
+    n_set = 106
     X, y = data.getSet(n_set)
     X_train = X[:int(n_set * (2 / 3))]
     y_train = y[:int(n_set * (2 / 3))]
     X_test = X[int(n_set * (2 / 3)):]
     y_test = y[int(n_set * (2 / 3)):]
     forestFit(X_train, y_train, X_test, y_test)
+
+    # data = Ion()
+    # n_set = 351
+    # X, y = data.getSet(n_set)
+    # X_train = X[:int(n_set * (2 / 3))]
+    # y_train = y[:int(n_set * (2 / 3))]
+    # X_test = X[int(n_set * (2 / 3)):]
+    # y_test = y[int(n_set * (2 / 3)):]
+    # forestFit(X_train, y_train, X_test, y_test)
 
     # n_set = 210
     # X, y = data.getSet(n_set)
