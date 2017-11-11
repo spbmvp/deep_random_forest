cb555d847745c3e08f1440a5b1d272db5f1827ea
Mikhail Popov <m.popov@crystals.ru>
2017-11-11 14:37:58 +0300

    fix log out and added compute sets

--- main.py
+++ main.py
@@ -16,15 +16,16 @@ def initLogging(logLevel):
                     level=logLevel)
 
 
-def forestFit(X_train, y_train, X_test, y_test):
-    log.info("Start set fitting")
+def forestFit(X_train, y_train, X_test, y_test, nameSet):
+    log.info("#######Start %s set fitting#########", nameSet)
+    log.info("     vi    MeanAcc  Med.Acc  Min acc  Max acc")
     Parallel(n_jobs=56)(delayed(in_parallel)(X_test, X_train, vi, y_test, y_train)for vi in frange(0, 1, 0.01))
-    log.info("Finish set fitting")
+    log.info("#########Finish %s set fitting##########", nameSet)
 
 
 def in_parallel(X_test, X_train, vi, y_test, y_train):
     acc_array = []
-    for i in range(50):
+    for i in range(100):
         cascade_forest = ForestsModel(n_trees_cf=int(X_train.shape[0] / 2)).get_forests()
         cascade_forest.fit(X_train, y_train, vi=vi, lamda=10 ** -12)
         pred = cascade_forest.predict(X_test)
@@ -36,8 +37,9 @@ def in_parallel(X_test, X_train, vi, y_test, y_train):
         acc_array.append(float(k / len(pred)))
     med = np.median(acc_array)
     mean = np.mean(acc_array)
-    log.info("\n\tvi = %f\n\tMean accuracy = %f\n\tMedian accuracy = %f\n\tMin accuracy = %f\n\tMax accuracy = %f",vi, mean, med,
-             np.min(acc_array), np.max(acc_array))
+    # log.info("\n\tvi = %f\n\tMean accuracy = %f\n\tMedian accuracy = %f\n\tMin accuracy = %f\n\tMax accuracy = %f",vi, mean, med,
+    #          np.min(acc_array), np.max(acc_array))
+    log.info(" %f %f %f %f %f", vi, mean, med, np.min(acc_array), np.max(acc_array))
 
 
 if __name__ == '__main__':
@@ -50,30 +52,31 @@ if __name__ == '__main__':
     y_train = y[:int(n_set * (2 / 3))]
     X_test = X[int(n_set * (2 / 3)):]
     y_test = y[int(n_set * (2 / 3)):]
-    forestFit(X_train, y_train, X_test, y_test)
+    forestFit(X_train, y_train, X_test, y_test, "Breast")
 
-    # data = Ion()
-    # n_set = 351
-    # X, y = data.getSet(n_set)
-    # X_train = X[:int(n_set * (2 / 3))]
-    # y_train = y[:int(n_set * (2 / 3))]
-    # X_test = X[int(n_set * (2 / 3)):]
-    # y_test = y[int(n_set * (2 / 3)):]
-    # forestFit(X_train, y_train, X_test, y_test)
+    data = Ion()
+    n_set = 351
+    X, y = data.getSet(n_set)
+    X_train = X[:int(n_set * (2 / 3))]
+    y_train = y[:int(n_set * (2 / 3))]
+    X_test = X[int(n_set * (2 / 3)):]
+    y_test = y[int(n_set * (2 / 3)):]
+    forestFit(X_train, y_train, X_test, y_test, "Ion")
 
-    # n_set = 210
-    # X, y = data.getSet(n_set)
-    # X_train = X[:int(n_set * (2 / 3))]
-    # y_train = y[:int(n_set * (2 / 3))]
-    # X_test = X[int(n_set * (2 / 3)):]
-    # y_test = y[int(n_set * (2 / 3)):]
-    # forestFit(X_train, y_train, X_test, y_test)
+    data = Seeds()
+    n_set = 210
+    X, y = data.getSet(n_set)
+    X_train = X[:int(n_set * (2 / 3))]
+    y_train = y[:int(n_set * (2 / 3))]
+    X_test = X[int(n_set * (2 / 3)):]
+    y_test = y[int(n_set * (2 / 3)):]
+    forestFit(X_train, y_train, X_test, y_test, "Seeds")
 
-    # data = Usps()
-    # n_set = 2000
-    # X, y = data.getSet(n_set)
-    # X_train = X[:int(n_set * (2 / 3))]
-    # y_train = y[:int(n_set * (2 / 3))]
-    # X_test = X[int(n_set * (2 / 3)):]
-    # y_test = y[int(n_set * (2 / 3)):]
-    # forestFit(X_train, y_train, X_test, y_test)
+    data = Usps()
+    n_set = 2000
+    X, y = data.getSet(n_set)
+    X_train = X[:int(n_set * (2 / 3))]
+    y_train = y[:int(n_set * (2 / 3))]
+    X_test = X[int(n_set * (2 / 3)):]
+    y_test = y[int(n_set * (2 / 3)):]
+    forestFit(X_train, y_train, X_test, y_test, "Usps")
